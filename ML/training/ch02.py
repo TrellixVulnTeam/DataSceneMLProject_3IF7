17a2509a57ba2065fa14c3a0503b38c257bbb7e1
Sim <https://github.com/VladiNaumov>
2022-02-09 23:53:25 +0200

    ML

--- ML/training/ch02.py
+++ ML/training/ch02.py
@@ -9,13 +9,14 @@ from sklearn.neighbors import KNeighborsClassifier
 # генерируем набор данных
 X, y = mglearn.datasets.make_forge()
 
-# строим график для набора данных
+
+# смотрим какие данные и строим график для набора данных
 mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
 plt.legend(["Класс 0", "Класс 1"], loc=4)
 plt.xlabel("Первый признак")
 plt.ylabel("Второй признак")
 print("форма массива X: {}".format(X.shape))
-# plt.show()
+plt.show()
 
 X, y = mglearn.datasets.make_wave(n_samples=40)
 plt.plot(X, y, 'o')
@@ -23,14 +24,17 @@ plt.ylim(-3, 3)
 plt.xlabel("Признак")
 plt.ylabel("Целевая переменная")
 
-#plt.show()
+plt.show()
+
 
 
 # Загружаем данные из scikit-learn с помощью  функции load_breast_cancer:
 cancer = load_breast_cancer()
+
 print("Ключи cancer(): \n{}".format(cancer.keys()))
 
 """
+OUT:
 dict_keys(['feature_names', 'data', 'DESCR', 'target', 'target_names']) 
 
     feature_names– это список строк с описанием  каждого признака
@@ -61,24 +65,35 @@ Out:
  'worst concave points' 'worst symmetry' 'worst fractal dimension'] 
  
  """
-# более  подробную  информацию о данных можно получить, прочитав cancer.DESCR.
+# что бы получить более подробную информацию о данных нужно прочитать cancer.DESCR.
 print("подробную  информацию:\n{}".format(cancer.DESCR))
 
+"""
+# для примера мы загружаем  ещё данные 
 # Загружаем данные из scikit-learn с помощью  функции load_breast_cancer:
 boston = load_boston()
+
 print("форма массива data для набора boston: {}".format(boston.data.shape))
 
-# более подробную информацию о данных можно получить, прочитав boston.DESCR
+# что бы получить более подробную информацию о данных нужно прочитать boston.DESCR
 print("подробную  информацию:\n{}".format(boston.DESCR))
+
+
+"""
+
 
 #  Набор данных c производными признаками можно загрузить с помощью функции load_extended_boston:
 X, y = mglearn.datasets.load_extended_boston()
+
 print("форма массива X: {}".format(X.shape))
 print("форма массива y: {}".format(y.shape))
 
+
 # Алгоритм k ближайших  соседей
-# используются три ближайших соседа (n_neighbors=3):
+
+# используются три ближайших соседа (n_neighbors=3) и смотрим это на графике
 mglearn.plots.plot_knn_classification(n_neighbors=3)
+plt.show()
 
 
 # мы разделим наши  данные на обучающий и тестовый наборы, чтобы  оценить обобщающую способность модели
@@ -93,56 +108,12 @@ clf.fit(X_train, y_train)
 Для каждой точки тестового набора он вычисляет ее ближайших соседей  в  обучающем  наборе
 и находит среди них наиболее часто встречающийся класс:
 """
+
 print("Прогнозы на тестовом наборе: {}".format(clf.predict(X_test)))
 
-"""
-Для оценки обобщающей способности модели мы вызываем метод score() с тестовыми данными и тестовыми метками: 
-"""
+
+#Для оценки обобщающей способности модели мы вызываем метод score() с тестовыми данными и тестовыми метками:
 print("Правильность на тестовом наборе: {:.2f}".format(clf.score(X_test, y_test)))
 
-# Мы видим, что наша модель имеет правильность 86%,
-# то есть модель правильно предсказала класс для 86% примеров тестового набора
-
-
-
-# АНАЛИЗ KNeighborsClassifier()
-
-fig, axes = plt.subplots(1, 3, figsize=(10, 3))
-
-for n_neighbors, ax in zip([1, 3, 9], axes):
-    # создаем объект-классификатор и подгоняем в одной строке
-    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
-    mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=.4)
-    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
-    ax.set_title("количество соседей:{}".format(n_neighbors))
-    ax.set_xlabel("признак 0")
-    ax.set_ylabel("признак 1")
-axes[0].legend(loc=3)
-# plt.show()
-
-print('*********************************\n...')
-cancer = load_breast_cancer()
-X_train, X_test, y_train, y_test = train_test_split(
-    cancer.data, cancer.target, stratify=cancer.target, random_state=66)
-
-training_accuracy = []
-test_accuracy = []
-# пробуем n_neighbors от 1 до 10
-neighbors_settings = range(1, 11)
-
-# АНАЛИЗ KNeighborsClassifier
-
-for n_neighbors in neighbors_settings:
-    # строим модель
-    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
-    clf.fit(X_train, y_train)
-    # записываем правильность на обучающем наборе
-    training_accuracy.append(clf.score(X_train, y_train))
-    # записываем правильность на тестовом наборе
-    test_accuracy.append(clf.score(X_test, y_test))
-
-plt.plot(neighbors_settings, training_accuracy, label="правильность на обучающем наборе")
-plt.plot(neighbors_settings, test_accuracy, label="правильность на тестовом наборе")
-plt.ylabel("Правильность")
-plt.xlabel("количество соседей")
-plt.legend()
+# Мы видим, что наша модель имеет правильность 0.86%,
+# то есть модель правильно предсказала класс для 0.86% примеров тестового набора
